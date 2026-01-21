fbbbf40e9c1d86ecaeaff4973922b7f4064dce2a
19yusufozcan <19yusufozcan@gmail.com>
2026-01-20 20:51:11 -0500

    add lab01 with all answers

--- week01/lab01.py
+++ week01/lab01.py
@@ -5,3 +5,24 @@
 #questions2
 myArray = [1,4,7,9]
 
+#Question3: Order of operations
+
+a = 1
+b = 2
+c = 3
+d = 4
+
+#Fully-bracketed Version of: e=a-b**c//d+a%c
+e = (a -((b**c) // d)) + (a%c)
+
+#question4
+temperature = 32.6
+print("Tempreture today is:  {:.3f} degrees Celsius".format(temperature))
+
+
+#question 5
+
+userAge = int(input("What is your age?"))
+userAge = userAge + 22
+print("Now showing the shop items filtered by age:",userAge)
+
